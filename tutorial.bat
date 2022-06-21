68ce3ead805ce546e0c18d90631950e213724281
GladfanIsHere <96852415+GladfanIsHere@users.noreply.github.com>
2022-06-21 13:00:51 -0500

    Updated tutorial.bat to translate it

--- tutorial.bat
+++ tutorial.bat
@@ -1,68 +1,68 @@
 @echo off
 cd %~dp0
 title MiniBatch Alpha Tutorial
-echo Welcome in MiniBatch Alpha!
-echo This is still a testing phase, so anything that could be shown in this version could not be on the final release
-echo And anything could change
+echo Bienvenido a MiniBatch Alpha!
+echo Esta es una version de prueba, y algunas cosas vistas aqui podrian no estar en el juego final
+echo Y todo podria cambiar
 timeout /t 2 /nobreak >nul
-echo Press any key to start MiniBatch
+echo Pulsa cualquier tecla para iniciar MiniBatch
 pause >nul
 cls
-echo You find yourself in an island, what do you remember?
+echo Te encuentras perdido en una isla, que recuerdas?
 timeout /t 2 /nobreak >nul
 echo.
-echo Nothing...
+echo Nada...
 timeout /t 2 /nobreak >nul
 echo.
-echo Don't you really remember anything? Do you remember how you got here?
+echo De verdad no recuerdas nada? Recuerdas como llegastes aqui?
 timeout /t 3 /nobreak >nul
 echo.
 echo No
-echo I don't even remember who you are
+echo Ni siquiera recuerdo quien eres
 timeout /t 3 /nobreak >nul
-echo Hey?
-echo You there?
+echo Oye?
+echo estas ahi?
 timeout /t 2 /nobreak >nul
 echo ...
 timeout /t 1 /nobreak >nul
-echo Uh, i guess if i just imagined this...
+echo Uh, ojala esto hubiera sido mi imaginacion...
 timeout /t 1 /nobreak >nul
-echo What do i have? I must survive
-echo I have
+echo Que es lo que tengo? debo sobrevivir
+echo Tengo
 timeout /t 2 /nobreak >nul
 cls
-echo You find yourself in an island, what do you remember?
+echo Te encuentras perdido en una isla, que recuerdas?
 echo.
-echo Nothing...
+echo Nada...
 echo.
-echo Don't you really remember anything? Do you remember how you got here?
+echo De verdad no recuerdas nada? Recuerdas como llegastes aqui?
 echo.
 echo No
-echo I don't even remember who you are
-echo Hey?
-echo You there?
+echo Ni siquiera recuerdo quien eres
+echo Oye?
+echo Estas ahi?
 echo ...
-echo Uh, i guess if i just imagined this...
-echo What do i have? I must survive
-echo I have...
+echo Uh, ojala todo esto hubiera sido mi imaginacion...
+echo Que es lo que tengo? debo sobrevivir
+echo Tengo...
 timeout /t 1 /nobreak >nul
 cls
-echo You find yourself in an island, what do you remember?
+echo ?
 echo.
-echo Nothing...
+echo Nada...
 echo.
-echo Don't you really remember anything? Do you remember how you got here?
+echo De verdad no recuerdas nada? Recuerdas como llegastes aqui?
 echo.
 echo No
-echo I don't even remember who you are
-echo Hey?
-echo You there?
+echo Ni siquiera recuerdo quien eres
+echo Oye?
+echo Estas ahi?
 echo ...
-echo Uh, i guess if i just imagined this...
-echo What do i have? I must survive
-echo I have a %picktype%, a %swordtype% and a %axetype%
+echo Uh, ojala hubiera sido mi imaginacion...
+echo Que es lo que tengo? debo sobrevivir
+echo Tengo un %picktype%, una %swordtype% y un %axetype%
 timeout /t 1 /nobreak >nul
-echo Press any key to continue
+echo Pulsa cualquier tecla para continuar
 pause >nul
 cls
 goto tutorial
@@ -70,11 +70,11 @@ goto tutorial
 :tutorial
 set /a tempvar=0
 cls
-echo In the alpha phase, you can't fight, chop and mine yourself, but the program does, randomly, depending on your tool
-echo When everything that must be done gets completed, you will be able to chop wood, fight enemies and mine in a cave
-echo For now, you will have a "lobby" where you choose what to do
+echo En la version alpha, no puedes pelear, talar y minar por ti mismo, pero el programa lo hace, aleatoriamente, dependiendo
+echo en tu herramienta, cuando todo lo que se deba hacer se haya hecho, podras talar madera, pelear contra enemigos
+echo y minar en una cueva, por ahora, tendras un "lobby" donde elegiras que hacer.
 timeout /t 5 /nobreak >nul
-echo Press any key to continue
+echo Pulsa cualquier tecla para continuar
 pause >nul
 goto aftertutorialtext
 
@@ -82,14 +82,14 @@ goto aftertutorialtext
 cls
 if %tempvar% equ 3 (
     cls
-    echo Tutorial ended!
+    echo Tutorial terminado!
     timeout /t 1 /nobreak >nul
     echo.
-    echo You will be sent shortly to normal MiniBatch
-    echo Enjoy!
+    echo Seras enviado en un ratito al MiniBatch normal
+    echo Disfruta!
     timeout /t 2 /nobreak >nul
     echo.
-    echo Starting MiniBatch...
+    echo Comenzando MiniBatch...
     set tempvar=
     if not defined tempvar (
         cd stats
@@ -102,9 +102,9 @@ if %tempvar% equ 3 (
         goto aftertutorialtext
     )
 )
-echo 1. Go mine
-echo 2. Go fight
-echo 3. Go chop
+echo 1. Ir a minar
+echo 2. Ir a pelear
+echo 3. Ir a talar
 choice /c 123 /m "What do you want to do?"
 if %errorlevel% equ 1 (
     goto minetutorial
@@ -118,86 +118,86 @@ if %errorlevel% equ 3 (
 
 :minetutorial
 cls
-echo Loots will change depending on your tool (and your luck)
-echo Press any key to continue
+echo El botin va a cambiar dependiendo en tu herramienta (y tu suerte)
+echo Pulsa cualquier tecla para continuar
 pause >nul
 cls
-echo Mining.
+echo Minando.
 timeout /t 1 /nobreak >nul
 cls
-echo Mining..
+echo Minando..
 timeout /t 1 /nobreak >nul
 cls
-echo Mining...
+echo Minando...
 timeout /t 1 /nobreak >nul
 cls
-echo You mined %mineloot% Iron ores with your %picktype%.
+echo Has minado %mineloot% Menas de hierro con tu %picktype%.
 timeout /t 2 /nobreak >nul
-echo Press any key to continue
+echo Pulsa cualquier tecla para continuar
 pause >nul
 set /a tempvar=1
 goto aftertutorialtext
 
 :fighttutorial
 cls
-echo Monster's loots will change depending on your tool (and your luck)
-echo Press any key to continue
+echo El botin de los monstruos va a cambiar dependiendo en tu herramienta (Y tu suerte)
+echo Pulsa cualquier tecla para continuar
 pause >nul
 cls
-echo Fighting.
+echo Peleando.
 timeout /t 1 /nobreak >nul
 cls
-echo Fighting..
+echo Peleando..
 timeout /t 1 /nobreak >nul
 cls
-echo Fighting...
+echo Peleando...
 timeout /t 1 /nobreak >nul
 cls
-echo You fought some monsters with your %swordtype% and got:
-echo %fightlootskeleton% bones from a skeleton
-echo %fightlootzombie% rotten flesh from a zombie
-echo %fightlootspider% strings from a spider
+echo Peleaste contra algunos monstruos con tu %swordtype% y obtuvistes:
+echo %fightlootskeleton% huesos de un esqueleto
+echo %fightlootzombie% carne podrida de un zombi
+echo %fightlootspider% hilo de un aracnido
 timeout /t 1 /nobreak >nul
-echo You also took %damage% damage
+echo Tu vida a bajado %damage%
 set /a currenthealth=%health%-%damage%
-echo Your health is %currenthealth%
+echo Tu vida esta a %currenthealth%
 set /a health=%currenthealth%
 if %health% lss 1 (
     timeout /t 1 /nobreak >nul
     cls
-    echo ::  ::   ::::::  ::    ::     ::::::  :: ::::::: ::::::  
-    echo ::  ::  ::    :: ::    ::     ::   :: :: ::      ::   :: 
-    echo  ::::   ::    :: ::    ::     ::   :: :: :::::   ::   :: 
-    echo   ::    ::    :: ::    ::     ::   :: :: ::      ::   :: 
-    echo   ::     ::::::   ::::::      ::::::  :: ::::::: ::::::  
+    echo ::    ::   ::::::::   ::::::::       ::      ::   ::      ::   ::::::   ::::::   ::::::     ::::::
+    echo ::    ::   ::    ::   ::             ::::  ::::   ::      ::   ::       ::  ::     ::     ::      ::
+    echo ::::::::   ::::::::   ::::::::       ::  ::  ::   ::      ::   ::::::   ::::::     ::     ::      ::
+    echo ::    ::   ::    ::         ::       ::      ::   ::      ::   ::       ::  ::     ::     ::      ::
+    echo ::    ::   ::    ::   ::::::::       ::      ::     ::::::     ::::::   ::    ::   ::       ::::::
     timeout /t 3 /nobreak >nul
-    echo In this alpha version, nothing will change if you die and your health will be back to 20
+    echo En esta version Alpha, nada pasara si mueres y tu vida volvera a ser 20
     set /a health=20
 )
 timeout /t 2 /nobreak >nul
-echo Press any key to continue
+echo Pulsa cualquier tecla para continuar
 pause >nul
 set /a tempvar=%tempvar%+1
 goto aftertutorialtext
 
 :choptutorial
 cls
-echo Wood loots will change depending on your tool (and your luck)
-echo Press any key to continue
+echo El botin de la madera dependera en tu herramienta (y tu suerte)
+echo Pulsa cualquier tecla para continuar
 pause >nul
 cls
-echo Chopping.
+echo Talando.
 timeout /t 1 /nobreak >nul
 cls
-echo Chopping..
+echo Talando..
 timeout /t 1 /nobreak >nul
 cls
-echo Chopping...
+echo Talando...
 timeout /t 1 /nobreak >nul
 cls
-echo You chopped %choplootwood% Wood Planks and %choplootapple% Apples with your %axetype%.
+echo Has talado %choplootwood% Tablas de madera y %choplootapple% Manzanas con tu %axetype%.
 timeout /t 2 /nobreak >nul
-echo Press any key to continue
+echo Pulsa cualquier tecla para continuar
 pause >nul
 set /a tempvar=%tempvar%+1
 goto aftertutorialtext
